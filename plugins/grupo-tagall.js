a2dba238c2c93fb3f8b48c01b18e8b5542e83a64
jerem146 <vasquezever146@gmail.com>
2025-08-03 11:58:24 -0500

    Actualizar grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -5,30 +5,16 @@
 */
 const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, command, usedPrefix }) => {
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
-
   const customEmoji = global.db.data.chats[m.chat]?.customEmoji || '🍫';
   m.react(customEmoji);
-
   if (!(isAdmin || isOwner)) {
     global.dfail('admin', m, conn);
     throw false;
   }
-
   const pesan = args.join` `;
   const oi = `*» INFO :* ${pesan}`;
-  let teks = `*!  MENCION GENERAL  !*\n  *PARA ${participants.length} MIEMBROS* 🗣️\n\n ${oi}\n\n╭  ┄ 𝅄 ۪꒰ \`⡞᪲=͟͟͞${botname} ≼᳞ׄ\` ꒱ ۟ 𝅄 ┄\n`;
-  for (const mem of participants) {
-    teks += `┊${customEmoji} @${mem.id.split('@')[0]}\n`;
-  }
-  teks += `╰⸼ ┄ ┄ ┄ ─  ꒰  ׅ୭ *${vs}* ୧ ׅ ꒱  ┄  ─ ┄ ⸼`;
-
+  let teks = `*[ ! ] Invocando a los integrantes del grupo*\n\n*${botname}* *~> Invocador* : _${m.sender.split('@')[0]}_\n*~> Mensaje* : _${pesan}_\n\n`;
+  teks += participants.map((mem) => `╠➥ @${mem.id.split('@')[0]}`).join('\n');
+  teks = `╔═══ஜ۩۞۩ஜ═══╗\n` + teks + `\n╚═══════════`;
   conn.sendMessage(m.chat, { text: teks, mentions: participants.map((a) => a.id) });
 };
-
-handler.help = ['todos *<mensaje opcional>*'];
-handler.tags = ['group'];
-handler.command = ['todos', 'invocar', 'tagall']
-handler.admin = true;
-handler.group = true;
-
-export default handler;
