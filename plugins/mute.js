c4140e7b96470156262c043f36594f2682ea917e
jerem146 <vasquezever146@gmail.com>
2025-08-03 12:23:11 -0500

    Actualizar mute.js

--- plugins/mute.js
+++ plugins/mute.js
@@ -1,12 +1,35 @@
-const handler = async (m, { conn, args }) => {
+const handlerMute = async (m, { conn, args }) => {
   const userId = m.mentionedJid[0] || args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net'
   if (!userId) return m.reply('Debes mencionar a un usuario o proporcionar su número de teléfono')
-  const user = global.db.data.users[userId]
-  if (!user) return m.reply('El usuario no existe en la base de datos')
+  let user = global.db.data.users[userId]
+  if (!user) {
+    user = global.db.data.users[userId] = {
+      muto: false,
+      // Agrega otras propiedades que necesites
+    }
+  }
   user.muto = true
-  m.reply(`El usuario ${user.name} ha sido muteado con éxito`)
+  m.reply(`El usuario ${userId} ha sido muteado con éxito`)
 }
 
-handler.command = ['muteado']
-handler.admin = true
-export default handler
+handlerMute.command = ['mute']
+handlerMute.admin = true
+
+const handlerUnmute = async (m, { conn, args }) => {
+  const userId = m.mentionedJid[0] || args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net'
+  if (!userId) return m.reply('Debes mencionar a un usuario o proporcionar su número de teléfono')
+  let user = global.db.data.users[userId]
+  if (!user) {
+    user = global.db.data.users[userId] = {
+      muto: false,
+      // Agrega otras propiedades que necesites
+    }
+  }
+  user.muto = false
+  m.reply(`El usuario ${userId} ha sido desmuteado con éxito`)
+}
+
+handlerUnmute.command = ['unmute']
+handlerUnmute.admin = true
+
+export { handlerMute, handlerUnmute }
