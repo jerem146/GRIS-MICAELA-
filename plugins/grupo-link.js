73ed2ece6f918c1b70bc4410c25318fc1b9f7a36
jerem146 <vasquezever146@gmail.com>
2025-08-08 11:35:39 -0500

    Actualizar grupo-link.js

--- plugins/grupo-link.js
+++ plugins/grupo-link.js
@@ -1,10 +1,15 @@
-var handler = async (m, { conn, args }) => {
+var handler = async (m, { conn }) => {
+  let group = m.chat
+  let code = await conn.groupInviteCode(group)
+  let link = 'https://chat.whatsapp.com/' + code
 
-let group = m.chat
-let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.reply(m.chat, '\t\t✿:･✧ link del grupo ✧･:✿\n\n\v' + link, m, { detectLink: true })
+  // Primer mensaje decorado
+  await conn.reply(m.chat, '⋆｡°✩ ɪɴᴠɪᴛᴀᴄɪᴏ́ɴ ᴅᴇʟ ɢʀᴜᴘᴏ ✩°｡⋆\n\n⬇️ Aquí tienes el enlace de este grupo:', m)
 
+  // Segundo mensaje: solo el link (para activar el preview del grupo)
+  await conn.sendMessage(m.chat, { text: link }, { quoted: m })
 }
+
 handler.help = ['link']
 handler.tags = ['grupo']
 handler.command = ['link', 'enlace']
