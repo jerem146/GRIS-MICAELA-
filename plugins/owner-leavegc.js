9eef101d70d503979396c348b31dba95a0e0e59b
jerem146 <vasquezever146@gmail.com>
2025-08-13 13:43:00 -0500

    Actualizar owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
 let chat = global.db.data.chats[m.chat]
 chat.welcome = false
-await conn.reply(id, `${emoji} Adios a todos, el Bot se despide! (≧ω≦)ゞ`) 
+await conn.reply(id, `${emoji} este grupo es una mrd, al igual que el creador y sus administradores son una mrd comam vrd 🖕🖕`) 
 await conn.groupLeave(id)
 try {  
 chat.welcome = true
