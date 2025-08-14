39d5ab88f4b44c36c290a0c7638035a7f1b4ee92
jerem146 <vasquezever146@gmail.com>
2025-08-14 07:41:41 -0500

    Actualizar grupo-config.js

--- plugins/grupo-config.js
+++ plugins/grupo-config.js
@@ -1,34 +1,11 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => icono) 
-
-    // Determinar acción: open o close
-    let action = (args[0] || '').toLowerCase()
-    let setting
-
-    if (action === 'open') setting = 'not_announcement'
-    else if (action === 'close') setting = 'announcement'
-    else {
-        return conn.reply(
-            m.chat,
-            `${emoji} *Elija una opción válida para configurar el grupo*\n\nEjemplo:\n` +
-            `*✰ ${usedPrefix}${command} open* - Permitir que todos escriban\n` +
-            `*✰ ${usedPrefix}${command} close* - Solo admins pueden escribir`,
-            m
-        )
-    }
-
-    await conn.groupSettingUpdate(m.chat, setting)
-
-    if (setting === 'not_announcement') {
-        m.reply(`${emoji} *El grupo está abierto. Todos pueden escribir.*`)
-    } else if (setting === 'announcement') {
-        m.reply(`${emoji2} *El grupo está cerrado. Solo los administradores pueden escribir.*`)
-    }
+let handler = async (m, { conn, command }) => {
+  const isClose = command === 'close' ? 'announcement' : 'not_announcement'
+  await conn.groupSettingUpdate(m.chat, isClose)
 }
 
-handler.help = ['group open / close']
+handler.help = ['open', 'close']
 handler.tags = ['grupo']
-handler.command = ['group', 'grupo']
+handler.command = ['open', 'close']
 handler.admin = true
 handler.botAdmin = true
 
