5e9c510af708e877b4ca15ab0f689084af668d23
jerem146 <vasquezever146@gmail.com>
2025-08-24 08:20:06 -0500

    Actualizar grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
 
 const handler = async (msg, { conn, args }) => {
   const rawID = conn.user?.id || "";
@@ -57,4 +57,4 @@ const handler = async (msg, { conn, args }) => {
 };
 
 handler.command = ["tagall", "invocar", "todos"];
-module.exports = handler;
+export default handler;
