b26444e2bb4a5937908e6aa26eb2504d5cc2c9b1
jerem146 <vasquezever146@gmail.com>
2025-08-24 13:31:57 -0500

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -2,46 +2,46 @@ import { sticker } from '../lib/sticker.js'
 import axios from 'axios'
 
 const fetchSticker = async (text) => {
-    const url = `https://api.neoxr.eu/brat`
-    const response = await axios.get(url, {
-        params: { text, apikey: 'Paimon' },
-        responseType: 'arraybuffer',
-        timeout: 20000
-    })
-    return response.data
+  const response = await axios.get('https://api.lolhuman.xyz/api/brat', {
+    params: { text },
+    responseType: 'arraybuffer',
+    timeout: 20000
+  })
+  return response.data
 }
 
 let handler = async (m, { conn, text }) => {
-    if (m.quoted) {
-        text = m.quoted.text || m.quoted.caption || text
-    }
-    if (!text) {
-        return conn.sendMessage(m.chat, {
-            text: `❀ Por favor, responde a un mensaje o ingresa un texto para crear el Sticker.`,
-        }, { quoted: m })
-    }
+  if (m.quoted) {
+    text = m.quoted.text || m.quoted.caption || text
+  }
+  if (!text) {
+    return conn.sendMessage(
+      m.chat,
+      { text: `❀ Por favor, responde a un mensaje o ingresa un texto para crear el Sticker.` },
+      { quoted: m }
+    )
+  }
 
-    try {
-        const buffer = Buffer.from(await fetchSticker(text))
-        let userId = m.sender
-        let packstickers = global.db.data.users[userId] || {}
-        let texto1 = packstickers.text1 || global.packsticker
-        let texto2 = packstickers.text2 || global.packsticker2
+  try {
+    const buffer = Buffer.from(await fetchSticker(text))
+    let userId = m.sender
+    let packstickers = global.db.data.users[userId] || {}
+    let texto1 = packstickers.text1 || global.packsticker
+    let texto2 = packstickers.text2 || global.packsticker2
 
-        // Generar sticker
-        let stiker = await sticker(buffer, false, texto1, texto2)
-
-        if (stiker) {
-            // Enviar sticker que se ve en WhatsApp
-            await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
-        } else {
-            throw new Error("✧ No se pudo generar el sticker.")
-        }
-    } catch (error) {
-        return conn.sendMessage(m.chat, {
-            text: `⚠︎ Ocurrió un error: ${error.message}`,
-        }, { quoted: m })
+    const stiker = await sticker(buffer, false, texto1, texto2)
+    if (stiker) {
+      await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+    } else {
+      throw new Error("✧ No se pudo generar el sticker.")
     }
+  } catch (error) {
+    return conn.sendMessage(
+      m.chat,
+      { text: `⚠︎ Ocurrió un error: ${error.message}` },
+      { quoted: m }
+    )
+  }
 }
 
 handler.command = ['brat']
