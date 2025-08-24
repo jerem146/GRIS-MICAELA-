0da2edf27342e3c3b0172c1ef35746e4e36a2961
jerem146 <vasquezever146@gmail.com>
2025-08-24 14:08:57 -0500

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -2,32 +2,7 @@ import { sticker } from '../lib/sticker.js'
 import axios from 'axios'
 
 const bratMaker = async (text) => {
-  text = text.toLowerCase()
-  const config = {
-    type: 'bar',
-    data: {
-      labels: [''],
-      datasets: [{
-        label: text,
-        data: [1],
-        backgroundColor: '#8ACE00'
-      }]
-    },
-    options: {
-      plugins: {
-        legend: {
-          display: true,
-          labels: {
-            font: { family: 'Impact', size: 120, weight: 'bold' },
-            color: '#000000'
-          }
-        }
-      },
-      scales: { x: { display: false }, y: { display: false } }
-    }
-  }
-  const url = `https://quickchart.io/chart?c=${encodeURIComponent(JSON.stringify(config))}&w=512&h=512&f=png`
-
+  const url = `https://fakeimg.pl/512x512/8ACE00/000000/?font=impact&font_size=70&text=${encodeURIComponent(text)}`
   const resp = await axios.get(url, { responseType: 'arraybuffer' })
   return resp.data
 }
